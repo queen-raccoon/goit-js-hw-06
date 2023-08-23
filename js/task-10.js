52fa7ca222ef4dfe2d221501e658988380b80515
queen-raccoon <k.i.d.v.s.k.a.t.2.1.3@gmail.com>
2023-08-23 20:02:01 +0400

    Update task - 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,20 @@
+ const getDivControls = document.querySelector("#controls");
+const inputEl = getDivControls.getElementsByTagName("input");
+const buttonCreate = getDivControls.querySelector("[data-create]");
+const buttonDestroy = getDivControls.querySelector("[data-destroy]");
+const getDivBoxes = document.querySelector("#boxes");
+
+
+const createBoxes = (amount) => {
+  
+  
+};
+
+getDivControls.addEventListener("click", createBoxes);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
